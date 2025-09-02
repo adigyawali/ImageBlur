0b42d497b26e6a81d4ef05988aafc1a6535a25fc
Aditya Gyawali <gyawaliaditya16@gmail.com>
2025-09-01 18:37:10 -0600

    updates

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 """
 From an Image, do gaussian blur manually and output result
 """
+
+from PIL import Image
+import numpy as np
+
+
+imageFile = "tokyoimg.jpg"
+
+def main():
+    img = Image.open(imageFile)
+    arr = np.array(img)
+
+    #Check the image shape
+
+    print(f"Image Shape: {arr.shape}")
+
+
+
+if __name__ == "__main__":
+    main()
